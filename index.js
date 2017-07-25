f19fe22aa61b06f99b77c5f4b76b697118a3251f
Cam <cam@mackay.net.nz>
2017-07-25 14:49:33 +1200

    Revert "fix to roman"
    This reverts commit 43eb4788b1ff76f0ec87f19ea73b094f321c2f9f.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const numerals = {
-  'M': { 'value': 1000, 'prefix': 'C' },
-  'D': { 'value': 500, 'prefix': 'C' },
-  'C': { 'value': 100, 'prefix': 'X' },
-  'L': { 'value': 50, 'prefix': 'X' },
-  'X': { 'value': 10, 'prefix': 'I' },
-  'V': { 'value': 5, 'prefix': 'I' },
-  'I': { 'value': 1 }
+  'M': 1000,
+  'D': 500,
+  'C': 100,
+  'L': 50,
+  'X': 10,
+  'V': 5,
+  'I': 1
 };
 
 function isRoman(text) {
@@ -17,22 +17,11 @@ function isRoman(text) {
 
 function toRoman(arabic) {
   var roman = '';
-  for (var key in numerals) {
-    var numeral = numerals[key];
-    var value = numeral.value;
+  for (var numeral in numerals) {
+    var value = numerals[numeral];
     for (var i = arabic - value; i >= 0; i -= value) {
-      roman += key;
+      roman += numeral;
       arabic -= value;
-    }
-    if (numeral.hasOwnProperty('prefix')) {
-      var prefixNumeral = numerals[numeral.prefix];
-      var prefixValue = prefixNumeral.value;
-      var com = numeral.prefix + key;
-      var val = value - prefixValue;
-      for (var i = arabic - val; i >= 0; i -= val) {
-        roman += com;
-        arabic -= val;
-      }
     }
   }
   return roman;
@@ -41,7 +30,7 @@ function toRoman(arabic) {
 function toArabic(roman) {
   var arabic = 0;
   Array.from(roman).reduceRight((previous, current) => {
-    var value = numerals[current].value;
+    var value = numerals[current];
     arabic = value < previous ? arabic - value : arabic + value;
     return value;
   }, 0);
